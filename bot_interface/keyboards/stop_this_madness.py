295d520f1beb3aec0355c87a0b8e915943e4f696
Andrey Samofalov <samofand@yandex.ru>
2022-11-25 15:39:52 +0300

    удалена лишняя функция, добавлены docstrings

--- bot_interface/keyboards/stop_this_madness.py
+++ bot_interface/keyboards/stop_this_madness.py
@@ -2,13 +2,9 @@ from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 
 
 def keyboard(states: list[str]):
-
+    """ Функция для создания клавиатуры ответа """
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     buttons = [KeyboardButton(text=key) for key in states]
     markup.add(*buttons)
 
     return markup
-
-
-def stop_this():
-    return keyboard(['В начало'])
