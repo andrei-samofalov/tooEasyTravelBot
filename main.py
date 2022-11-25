b67fe6326b5eef6f4463fd3d7ddabccf7b9aa8e8
Andrey Samofalov <samofand@yandex.ru>
2022-11-25 15:53:48 +0300

    добавлена инфо запись

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ from loader import bot
 if __name__ == '__main__':
     base_commands(bot)
     bot.add_custom_filter(StateFilter(bot))
+    print('Бот запущен')
     bot.polling(non_stop=True, interval=0)
 
