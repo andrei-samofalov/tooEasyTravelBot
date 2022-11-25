798168fa376a7ce098e795b7442ab6b2bdc26a4e
Andrey Samofalov <samofand@yandex.ru>
2022-11-25 13:49:37 +0300

    docstrings

--- database/data_load.py
+++ database/data_load.py
@@ -1,17 +1,40 @@
 import json
 
 
-def new_user(user_id):
+def new_user(user_id: str | int) -> dict:
+    """
+    Функция для создания словаря-заготовки с ID пользователя для внесения в БД
+    :param user_id: str | int, ID пользователя в Телеграм
+    :return: dict, словарь-заготовка для записи в базу данных
+    """
     log = {'id': user_id, 'requests': []}
     return log
 
 
-def load_to_dict(user_dict, command, time, data_list):
-    user_dict['requests'] += {'Команда': command, 'Дата и время вызова': time, 'Результат': '\n'.join(data_list)},
+def load_to_dict(user_dict: dict, command: str, time: str, data_list: list) -> dict:
+    """
+    Функция для записи запрошенных отелей в словарь, созданный в функции new_user
+
+    :param user_dict: dict, из new_user
+    :param command: str, команда, которую пользователь вызвал
+    :param time: str, строковое представление времени вызова команды
+    :param data_list: list, данные, полученные от API
+    :return: dict
+    """
+    user_dict['requests'] += {'Команда': command,
+                              'Дата и время вызова': time,
+                              'Результат': '\n'.join(data_list)
+                              },
     return user_dict
 
 
-def load_to_json(user_id, user_dict: dict):
+def load_to_json(user_id: str | int, user_dict: dict) -> None:
+    """
+    Функция для дампа данных из словаря user_dict в json-файл
+    :param user_id: str | int, ID пользователя Телеграм
+    :param user_dict: dict, словарь с собранными данными
+    :return: None
+    """
     try:
         with open('database/data_base.json', 'r', encoding='utf-8') as db:
             json_db = json.load(db)
@@ -30,6 +53,3 @@ def load_to_json(user_id, user_dict: dict):
                 json_db += user_dict,
 
             json.dump(json_db, db, indent=4)
-
-
-
