1698de9a8ab76457560ad0ccbd31750351cf9d1f
Andrey Samofalov <samofand@yandex.ru>
2022-11-25 22:34:07 +0300

    echo message added

--- bot_interface/on_echo.py
+++ bot_interface/on_echo.py
@@ -1,6 +1,7 @@
 from telebot.types import Message
 
 from loader import bot
+from settings.config import ECHO_MESSAGE
 
 
 @bot.message_handler(content_types=['text'])
@@ -8,4 +9,4 @@ def echo(message: Message) -> None:
     """ Хэндлер, реагирует на любые сообщения пользователя вне опроса
         """
     bot.send_message(chat_id=message.from_user.id,
-                     text='Проверьте ввод, для справки /help')
+                     text=ECHO_MESSAGE)
