b8663fc42c935c207ad4b7386d5c5ad492560eab
Andrey Samofalov <samofand@yandex.ru>
2022-11-25 22:33:42 +0300

    time of start added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from telebot.custom_filters import StateFilter
 
 from bot_interface.base_commands import base_commands
@@ -6,5 +8,5 @@ from loader import bot
 if __name__ == '__main__':
     base_commands(bot)
     bot.add_custom_filter(StateFilter(bot))
-    print('Бот запущен')
+    print(f'Бот запущен в {time.strftime("%H:%M:%S")}')
     bot.polling(non_stop=True, interval=0)
