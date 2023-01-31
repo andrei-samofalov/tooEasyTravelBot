dfabaebc8333f0626967c3f7006d857e9b857411
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-02-01 02:55:55 +0300

    level of logger, //need to set up by dict

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import logging
+
 from telebot.custom_filters import StateFilter
 
-from bot_interface.base_commands import base_commands
+from bot_interface import base_commands
 from loader import bot
 
 if __name__ == '__main__':
     base_commands(bot)
     bot.add_custom_filter(StateFilter(bot))
-    bot.polling(non_stop=True, interval=0)
+    bot.polling(non_stop=True, logger_level=logging.ERROR)
