f3e72005267487ef19789409c51e4e954f7287de
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-01-31 20:25:44 +0300

    init db

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,7 @@
 from . import data_load
+from .database_comands import *
+
+
+db_connection = sqlite3.Connection('./database/travel_bot.db', check_same_thread=False)
+cursor = db_connection.cursor()
+create_user_table(cursor)
