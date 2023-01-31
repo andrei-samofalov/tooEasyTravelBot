1b760edf4c39109cf9c370d485ae88086cefd41d
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-01-31 21:25:18 +0300

    create table with users queries named `sessions` somehow

--- database/__init__.py
+++ database/__init__.py
@@ -4,4 +4,5 @@ from .database_comands import *
 
 db_connection = sqlite3.Connection('./database/travel_bot.db', check_same_thread=False)
 cursor = db_connection.cursor()
-create_user_table(cursor)
+create_table_users(cursor)
+create_table_sessions(cursor)
