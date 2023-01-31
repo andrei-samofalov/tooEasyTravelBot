a0fed5f61f41371db4b91742351d97c11adc778e
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-01-31 21:27:21 +0300

    core db commands

--- database/database_comands.py
+++ database/database_comands.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-def create_user_table(c: sqlite3.Cursor) -> None:
+def create_table_users(c: sqlite3.Cursor) -> None:
     c.executescript("""
     begin transaction;
     CREATE TABLE IF NOT EXISTS `users` (
@@ -14,19 +14,37 @@ def create_user_table(c: sqlite3.Cursor) -> None:
     """)
 
 
-def create_session(c: sqlite3.Cursor) -> None:
+def create_table_sessions(c: sqlite3.Cursor) -> None:
     c.executescript("""
     create table if not exists `sessions` (
-    user_id integer,
+    id integer primary key autoincrement,
+    user_id integer references users(id),
     command text,
-    
-    
+    query_time `timestamp`,
+    destination_id integer,
+    destination_name text,
+    check_in `timestamp`,
+    check_out `timestamp`,
+    min_price integer,
+    max_price integer,
+    distance integer,
+    offer_amount integer,
+    photo_amount integer
     )
     """)
 
 
-add_user_sql = """
-    insert or ignore into users (id, first_name, last_name, username, language_code) 
+ADD_USER_TO_DB_SQL = """
+    insert or ignore into users (id, first_name, last_name, 
+                                username, language_code) 
     VALUES (?, ?, ?, ?, ?) 
-    """
+"""
 
+ADD_USER_REQUEST_TO_DB_SQL = """
+    insert into `sessions` (user_id, command, query_time, 
+                            destination_id, destination_name, 
+                            check_in, check_out, min_price,
+                            max_price, distance ,offer_amount, 
+                            photo_amount)
+    values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+"""
