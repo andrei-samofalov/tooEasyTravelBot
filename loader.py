7bb18b3c059a1752ddf5593d8a6ed7b7bb6ab0e7
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-02-01 02:54:58 +0300

    -2 rows of code

--- loader.py
+++ loader.py
@@ -3,10 +3,8 @@ from telebot.storage.memory_storage import StateMemoryStorage
 
 from settings import BOT_TOKEN
 
-state_storage = StateMemoryStorage()
-
 bot = telebot.TeleBot(
     BOT_TOKEN,
     parse_mode='html',
-    state_storage=state_storage
+    state_storage=StateMemoryStorage()
 )
