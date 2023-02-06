e186e33da4e3cc9240d73b884c8bcbb4ca67e877
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-02-06 22:27:47 +0300

    another try to implement model Hotel

--- API/models.py
+++ API/models.py
@@ -1,16 +1,18 @@
-import json
+import time
+from threading import Thread
+from typing import Any
 
 from jsonpointer import resolve_pointer
 
 from settings import logger
+from .get_images_request import get_hotel_details
 
 
-class Hotel:
+class Hotel(Thread):
     _points = {
         'id': '/id',
         'name': '/name',
-        'rating': '/star',
-        'availability': '/availability/minRoomsLeft',
+        'stars': '/star',
         'distance': '/destinationInfo/distanceFromDestination/value',
         'user_rating': '/reviews/score',
         'user_rates': '/reviews/total',
@@ -25,56 +27,56 @@ class Hotel:
         'add_description': '/data/propertyInfo/summary/location/whatsAround/editorial/content/0',
         'images': '/data/propertyInfo/propertyGallery/images',
     }
+    _struct_data: dict
+    _more_data: dict
+    _address: str
+    _images: list
+    _images_url: list
 
     def __init__(self, hotel_data: dict) -> None:
+        super().__init__()
         self._data = hotel_data
-        self._struct_data = self._resolve_data()
 
-    def _resolve_data(self) -> dict[str, str]:
+        logger.debug(f'{self.name}: {self._struct_data=}')
+        logger.debug(f'{self.name}: {self._address=}')
+        logger.debug(f'{self.name}: {self._images_url=}')
+
+    def run(self) -> None:
+        self._struct_data = self._resolve_data()
+        logger.debug(f'{self.name}: {self._struct_data=}')
+        self._more_data = self._get_additional_hotel_data()
+        logger.debug(f'{self.name}: {self._more_data=}')
+        self._resolve_images()
+        logger.debug(f'{self.name}: {self._images_url=}')
+        self._resolve_address()
+        logger.debug(f'{self.name}: {self._address=}')
+
+    def display_data(self) -> str:
+        return "\n".join(
+            f"<b>{data}</b>: {value}"
+            for data, value in self._struct_data.items()
+        )
+
+    def _resolve_data(self) -> dict[str, Any]:
         return {
             point: resolve_pointer(self._data, self._points[point])
             for point in self._points.keys()
         }
 
-    def display_data(self) -> list[str]:
-        return [
-            f"<b>{data}</b>: {value}"
-            for data, value in self._struct_data.items()
+    def _get_additional_hotel_data(self) -> dict:
+        return get_hotel_details(self._struct_data.get('id'))
+
+    def _resolve_images(self):
+        self._images: list[dict] = resolve_pointer(self._more_data, self._add_points.get('images'))
+        self._images_url: list[str] = [
+            resolve_pointer(img, '/image/url')
+            for img in self._images
         ]
 
-    def _resolve_images(self, hotel_details: dict) -> list[str]:
-        images: list[dict] = resolve_pointer(hotel_details, self._add_points.get('images'))
-        img_url = resolve_pointer(images, '/image/url')
-        return [img_url for _ in images]
-
-    def get_images(self):
-        pass
+    def _resolve_address(self):
+        self._address = resolve_pointer(self._more_data, self._add_points['address'])
 
 
 class HotelsRequest:
     def __init__(self):
         pass
-
-
-if __name__ == '__main__':
-    with open('prop_v2_list.json') as f:
-        hotel = json.load(f)
-        hot = Hotel(hotel)
-        _f = '\n'.join(hot.display_data())
-        print(_f)
-
-# https://www.hotels.com/Hotel-Search?
-# adults=2&
-# d1=2023-02-20&
-# d2=2023-02-21&
-# destination=Astana%2C%20Kazakhstan&
-# endDate=2023-02-21&
-# latLong=51.160516%2C71.470365&
-# regionId=602528&
-# selected=&
-# semdtl=&
-# sort=DISTANCE&
-# startDate=2023-02-20&
-# theme=&
-# useRewards=false&
-# userIntent=
