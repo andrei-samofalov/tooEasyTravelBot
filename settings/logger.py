eccb62921f2f26b773350ce5eef84aa9a3b0c3b3
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-02-06 22:26:31 +0300

    baseConfig deleted

--- settings/logger.py
+++ settings/logger.py
@@ -1,16 +1,6 @@
 import logging.config
 import sys
 
-
-def set_logger(name: str) -> logging.Logger:
-    logging.basicConfig(
-        level=logging.INFO,
-        format='[%(asctime)s %(module)s %(levelname)s] %(message)s',
-        datefmt='%d-%m-%Y %H:%M:%S'
-    )
-    return logging.getLogger(name)
-
-
 setup = {
     "version": 1,
     "disable_existing_loggers": True,
