a53436a1a9742ad2bb24c78d6f6c8426e0c804b7
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-02-06 19:16:26 +0300

    dict setup

--- settings/logger.py
+++ settings/logger.py
@@ -1,13 +1,52 @@
-import logging
+import logging.config
+import sys
 
 
 def set_logger(name: str) -> logging.Logger:
     logging.basicConfig(
-        level=logging.ERROR,
+        level=logging.INFO,
         format='[%(asctime)s %(module)s %(levelname)s] %(message)s',
         datefmt='%d-%m-%Y %H:%M:%S'
     )
     return logging.getLogger(name)
 
 
-logger = set_logger(__name__)
+setup = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "base": {
+            "format": "[%(asctime)s %(module)s %(levelname)s] %(message)s",
+            "datefmt": '%d-%m-%Y %H:%M:%S'
+        },
+        "extended": {
+            "format": "%(asctime)s %(levelname)s | %(module)s [line %(lineno)s]: %(message)s",
+            "datefmt": '%d-%m-%Y %H:%M:%S'
+        },
+    },
+    "handlers": {
+        "screen": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "base",
+            "stream": sys.stdout
+        },
+        "file": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "when": "midnight",
+            "backupCount": 5,
+            "formatter": "extended",
+            "level": "DEBUG",
+            "filename": "./settings/logs/tooEasyLog.log"
+        }
+    },
+    "loggers": {
+        "travelBotLogger": {
+            "level": "DEBUG",
+            "handlers": ["screen", "file"],
+         }
+    },
+}
+
+logging.config.dictConfig(setup)
+logger = logging.getLogger('travelBotLogger')
