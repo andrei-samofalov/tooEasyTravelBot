395816d7ceec11c19418bc6eb47f3d51fe994976
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-02-06 22:26:04 +0300

    logging added

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from telebot.custom_filters import StateFilter
 
 from bot_interface import base_commands
 from loader import bot
+from settings import logger
 
 if __name__ == '__main__':
     base_commands(bot)
     bot.add_custom_filter(StateFilter(bot))
+    logger.debug('Starting...')
     bot.infinity_polling()
