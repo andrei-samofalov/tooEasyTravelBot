60e08c27f24111aabf7380514a6fdfd16304290b
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-02-07 11:45:58 +0300

    rename module

--- API/__init__.py
+++ API/__init__.py
@@ -1,6 +1,6 @@
 from .api_functions import *
 from .city_search_request import *
 from .display_result import *
-from .get_images_request import *
+from .get_details_request import *
 from .hotel_search_request import *
 from .models import *
