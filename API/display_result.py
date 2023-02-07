f27cb8c70b12e41382431890278f141899fdf6c2
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-02-07 19:42:04 +0300

    indents

--- API/display_result.py
+++ API/display_result.py
@@ -33,33 +33,33 @@ def display_results(user_id: int) -> None:
             min_price=request_dict.get('Минимальная цена'),
         )
 
-        if not results:
-            bot.send_message(
-                chat_id=user_id,
-                text='По запросу ничего не найдено.\n' + ECHO_MESSAGE
-            )
-            return
+    if not results:
+        bot.send_message(
+            chat_id=user_id,
+            text='По запросу ничего не найдено.\n' + ECHO_MESSAGE
+        )
+        return
 
-        for hotel in results:
-            hotel.join()
-            display_hotel: str = hotel.display_data()
-            photos = request_dict.get('Кол-во фотографий')
+    for hotel in results:
+        hotel.join()
+        photos = request_dict.get('Кол-во фотографий')
 
-            if photos:
-                hotel_hotel_and_photos = hotel.display_with_photos(photos)
-                logger.debug(f'Starting sending messages after {time.time() - start:.3} sec')
-                bot.send_media_group(chat_id=user_id, media=hotel_hotel_and_photos)
-                continue
-
+        if photos:
+            hotel_hotel_and_photos = hotel.display_with_photos(photos)
             logger.debug(f'{hotel.name}: starting sending messages after {time.time() - start:.3} sec')
-            bot.send_message(
-                chat_id=user_id,
-                text=display_hotel,
-                disable_web_page_preview=True)
+            bot.send_media_group(chat_id=user_id, media=hotel_hotel_and_photos)
+            continue
 
-        else:
-            logger.info(f'All results have been displayed after {time.time() - start:.3} sec')
-            bot.send_message(
-                chat_id=user_id,
-                text='Все результаты выгружены.\n' + ECHO_MESSAGE
-            )
+        display_hotel: str = hotel.display_data()
+        logger.debug(f'{hotel.name}: starting sending messages after {time.time() - start:.3} sec')
+        bot.send_message(
+            chat_id=user_id,
+            text=display_hotel,
+            disable_web_page_preview=True)
+
+    else:
+        logger.info(f'All results have been displayed after {time.time() - start:.3} sec')
+        bot.send_message(
+            chat_id=user_id,
+            text='Все результаты выгружены.\n' + ECHO_MESSAGE
+        )
