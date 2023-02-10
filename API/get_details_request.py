6caba23997d4599b9f5206e6b42f85bccedc22d8
andrei-samofalov <andrei.e.samofalov@gmail.com>
2023-02-10 11:09:52 +0300

    logging changes

--- API/get_details_request.py
+++ API/get_details_request.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 
 from settings import headers, logger, url_hotel_details
@@ -11,7 +13,9 @@ def get_hotel_details(hotel_id: int) -> dict:
         "siteId": 300000001,
         "propertyId": hotel_id
     }
+    logger.debug(f'Hotel ID.{hotel_id}: additional data requested')
+    start = time.time()
     req = requests.request("POST", url_hotel_details, json=payload, headers=headers).json()
 
-    logger.debug(f'Hotel ID.{hotel_id}: additional data received')
+    logger.debug(f'Hotel ID.{hotel_id}: additional data received after {time.time() - start:.3} sec')
     return req
